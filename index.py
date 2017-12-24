899e784f2f52fc2bd7ce5b30385c963599ed7c64
Salomone Baquis <salomone.l.baquis@gmail.com>
2017-12-24 16:54:58 -0500

    gettin close

--- index.py
+++ index.py
@@ -1,9 +1,5 @@
 import csv
-import pprint
 import pandas as pd
-from itertools import groupby
-
-pp = pprint.PrettyPrinter(indent=2)
 
 # Match and filter national zip code data with our target zips
 def merge_zips(): 
@@ -25,81 +21,36 @@ def merge_plans():
 def dedupe_plans(): 
 	all_silver_plans = merge_plans()
 	unique_silver_plans = all_silver_plans.drop_duplicates(subset=["zipcode", "state", "rate_area", "rate"])
-	# unique_sorted_silver_plans = unique_silver_plans.sort_values(by=["state", "rate_area", "rate"])
 	return unique_silver_plans
 
+# Group, sort, and filter our plans to match target zips
 def filter_plans(): 
-	# Parses zips with multiple rate areas
-	def filter_ambiguous_zips(group):
+	# Empty rate values for zips with multiple rate areas
+	def identify_ambiguous_zips(group):
 		zipped_group = zip(group["zipcode"], group["rate_area"])
 		unique_zipped_group = list(set(zipped_group))
-		if len(unique_zipped_group) < 2: 
-			return True
-		else:
-			return False
-
+		if len(unique_zipped_group) > 1: 
+			group["rate"] = ""
+		return group
+	# Sort plans in each group by rate
+	def sort_rates_ascending(group):
+		group = group.sort_values(by=["rate"])
+		return group
 
 	plans = dedupe_plans()
-	grouped_plans = plans.groupby(["zipcode"]).filter(
-			lambda x: filter_ambiguous_zips(x)
-		)
-	# grouped_plans.to_csv("plan_test2.csv")
-	print(grouped_plans)
-	
+	grouped_plans = plans.groupby("zipcode", as_index=False)
+	modified_plans = grouped_plans.apply(identify_ambiguous_zips)
+	sorted_plans = modified_plans.groupby(["zipcode"], as_index=False).apply(sort_rates_ascending)
+	final_plans = sorted_plans.groupby("zipcode").nth(1).reset_index()
+	return final_plans
 
-			
-		
-
-	# for key, group in grouped_plans:
-	# 	print(group["rate_area"].unique(), group["zipcode"].unique())
-
-	# for item, group in grouped_plans:
-	# 	print((group["zipcode"], group["rate_area"]))
-		
-
+def match_final_plans(): 
+	final_plans = filter_plans()
+	target_zips = pd.read_csv("slcsp.csv")
+	target_zips = target_zips.dropna(axis=1)
+	merged_plans = target_zips.merge(final_plans, how="left", on="zipcode")
+	print(merged_plans)
 
 if __name__ == '__main__':
-	filter_plans()
-# def get_target_zips():
-# 	with open("slcsp.csv", newline="") as slcsp:
-# 		reader = csv.DictReader(slcsp)
-# 		zips = [zip["zipcode"] for zip in reader]
-# 		return zips
-
-# def get_state_data(): 
-# 	target_zips = get_target_zips()
-# 	with open ("zips.csv", newline="") as all_zips:
-# 		reader = csv.DictReader(all_zips)
-# 		state_data = [(zip["zipcode"], zip["state"], zip["rate_area"]) for zip in reader if zip["zipcode"] in target_zips]
-# 		return state_data
-		
-# def get_all_plans():
-# 	state_data = get_state_data()
-# 	# return state_data
-# 	with open ("plans.csv", newline="") as all_plans:
-# 		reader = csv.DictReader(all_plans)
-# 		for entry in state_data:
-# 			return
-
-		# plan_data = [
-		# 							(plan["state"], plan["rate_area"], plan["rate"], plan["metal_level"]) 
-		# 							for plan in reader 
-		# 							if (plan["state"], plan["rate_area"]) in state_data and plan["metal_level"] == "Silver"
-		# 						]
-		# # remove duplicate plans						
-		# plan_data = set(plan_data)					
-		# # sort our data by state and rate	
-		# plan_data = sorted(plan_data, key = lambda item : (item[0], item[1], item[2]))	
-
-		# return plan_data
-
-# def sort_plans():
-# 	unique_plans = get_all_plans()
-# 	correct_plans = []
-# 	for key, group in groupby(unique_plans, lambda x: (x[0], x[1])):
-# 		print(key)
-# 		group = list(group)
-# 		pp.pprint(group)
-# 		print(group[1])
-		
+	match_final_plans()
 
